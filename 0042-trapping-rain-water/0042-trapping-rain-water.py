a963a5fdffd4b82d72846865a8502df9633df4ab
Dheeraj Singh <145746734+Dheeraj261708@users.noreply.github.com>
2026-08-17 14:42:01 +0530

    Time: 3 ms (98.31%), Space: 13.6 MB (21.08%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -4,16 +4,39 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        stack=[]
-        water=0
-        for i in range(len(height)):
-            while stack and height[i]>height[stack[-1]]:
-                x=stack.pop()
-                if not stack:
-                    break
-                d=i-stack[-1]-1
-                min_height=min(height[i],height[stack[-1]])
-                water=water+d*(min_height-height[x])
-            stack.append(i)
+        # stack=[]
+        # water=0
+        # for i in range(len(height)):
+        #     while stack and height[i]>height[stack[-1]]:
+        #         x=stack.pop()
+        #         if not stack:
+        #             break
+        #         d=i-stack[-1]-1
+        #         min_height=min(height[i],height[stack[-1]])
+        #         water=water+d*(min_height-height[x])
+        #     stack.append(i)
+        # return water
+
+
+        left = 0
+        right = len(height) - 1
+        leftmax = 0
+        rightmax = 0
+        water = 0
+        
+        while left <= right:
+            if height[left] <= height[right]:
+                if height[left] >= leftmax:
+                    leftmax = height[left]
+                else:
+                    water += leftmax - height[left]
+                left += 1
+            else:
+                if height[right] >= rightmax:
+                    rightmax = height[right]
+                else:
+                    water += rightmax - height[right]
+                right -= 1
+                
         return water
         
